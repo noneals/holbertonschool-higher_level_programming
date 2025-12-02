5676cec8c8aa56e003f322b168cc917bc42b4566
noneals <124405266+noneals@users.noreply.github.com>
2025-12-02 20:08:04 +0300

    Update 0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,30 +1,50 @@
 #!/usr/bin/python3
-"""
+"""A module to add two numbers
 
-This module is composed by a function that adds two numbers
+This module performs the addition operation between two numbers,
+these numbers can be integers or floats.
 
 """
 
 
 def add_integer(a, b=98):
-    """ Function that adds two integer and/or float numbers
+    """Adds two numbers
+
+    Performs the addition between two numbers.
 
     Args:
-        a: first number
-        b: second number
+        a (:obj:`int, float`): The first number.
+        b (:obj:`int, float`, optional): The second number.
 
     Returns:
-        The addition of the two given numbers
-
-    Raises:
-        TypeError: If a or b aren't integer and/or float numbers
+        int: The result of the addition.
 
     """
+    if type(a) not in (int, float):
+        raise TypeError('a must be an integer')
 
-    if not isinstance(a, int) and not isinstance(a, float):
-        raise TypeError("a must be an integer")
-    if not isinstance(b, int) and not isinstance(b, float):
-        raise TypeError("b must be an integer")
-    a = int(a)
-    b = int(b)
-    return (a + b)
+    if type(b) not in (int, float):
+        raise TypeError('b must be an integer')
+
+    a = convert_to_int(a)
+    b = convert_to_int(b)
+    return a + b
+
+
+def convert_to_int(num):
+    """Cast the data type of num parameter
+
+    Convert a float number to a integer number
+
+    Args:
+        num (:obj:`int, float`): The number to cast.
+
+    Returns:
+        int: The number casted to integer.
+
+    """
+    if type(num) is float:
+        num = int(num)
+        return num
+
+    return num
