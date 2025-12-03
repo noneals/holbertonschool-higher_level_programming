ca8c9d6ced549c3d357331f9e6bf0d7407223e5b
noneals <124405266+noneals@users.noreply.github.com>
2025-12-03 16:40:56 +0300

    Update 5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,31 +1,25 @@
 #!/usr/bin/python3
 '''prints a text with 2 new lines after each
-    of these characters: ., ? and :'''
+of these characters: ., ? and :'''
 
 
 def text_indentation(text):
     '''prints a text with 2 new lines after each
     of these characters: ., ? and :'''
-    if type(text) is not str:
+    if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    '''Switch turns to 1 when a '.' '?' or ':' has been printed
-    to skip whitespaces'''
-    switch = 0
-    if text[0] == ' ':
-        switch = 1
-    for index in range(len(text)):
-        if text[index] in ['.', '?', ':']:
-            print(text[index], end="")
-            print('\n')
-            '''When a '.' '?' or ':' is found switch turns to 1'''
-            switch = 1
-        else:
-            if switch == 0:
-                print(text[index], end="")
-            elif switch == 1 and text[index] == ' ':
-                '''When switch is 1 we don't have to print  whitespaces'''
-                continue
-            else:
-                print(text[index], end="")
-                switch = 0
+    text = text.strip()
+
+    i = 0
+    while i < len(text):
+        char = text[i]
+        print(char, end="")
+        if char in ['.', '?', ':']:
+            print()
+            print()
+            i += 1
+            while i < len(text) and text[i] == ' ':
+                i += 1
+            i -= 1
+        i += 1
