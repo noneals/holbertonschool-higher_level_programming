00469f9043d3db09c97d2a9071a041e8a80d6db7
noneals <124405266+noneals@users.noreply.github.com>
2025-12-03 16:42:36 +0300

    Update 0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
+'''Adds 2 integer'''
 
 
 def add_integer(a, b=98):
-    """
-    adds an integer
-    unit tests located in tests/0-add_integer.txt
-    checks for type errors
-    """
-    if not isinstance(a, int):
-        if isinstance(a, float):
-            a = int(a)
-        else:
-            raise TypeError("a must be an integer")
-    if not isinstance(b, int):
-        if isinstance(b, float):
-            b = int(b)
-        else:
-            raise TypeError("b must be an integer")
-    return a + b
+    '''adds 2 integers.'''
+
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
