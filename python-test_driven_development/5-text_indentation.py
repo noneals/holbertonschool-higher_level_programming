4760a0287c377fe36000c9f961eef29ff05d8655
noneals <124405266+noneals@users.noreply.github.com>
2025-12-03 16:18:58 +0300

    Update 5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,36 +1,18 @@
 #!/usr/bin/python3
-"""
-Defines text_indentation
-"""
-
-
 def text_indentation(text):
-    """
-    Prints text with 2 new lines after '.', '?' and ':'
-    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    skip_space = False
+    new_line = False
+    for ch in text:
+        if new_line and ch == " ":
+            continue
 
-    for char in text:
+        print(ch, end="")
 
-        if skip_space:
-            # نحذف space فقط إذا كنا في بداية سطر جديد
-            if char == " ":
-                # لكن doctest يبغى نحافظ على المسافة داخل الجملة
-                # الحل: نخلي المسافة إذا السطر السابق مو فاصل
-                print(char, end="")
-                skip_space = False
-                continue
-            else:
-                print(char, end="")
-                skip_space = False
-                continue
-
-        print(char, end="")
-
-        if char in ".?:":
+        if ch in ".?:":
             print()
             print()
-            skip_space = True
+            new_line = True
+        else:
+            new_line = False
