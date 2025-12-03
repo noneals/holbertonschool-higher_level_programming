db5e89ea61f36ab7eca722a10e710b77e78b4c7a
noneals <124405266+noneals@users.noreply.github.com>
2025-12-03 18:59:48 +0300

    Update 0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 """
 Module that defines a function add_integer which adds two integers.
-"""
 
+Example:
+>>> add_integer(1, 2)
+3
+>>> add_integer(100, -2)
+98
+>>> add_integer(2)
+100
+>>> add_integer(100.3, -2)
+98
+>>> add_integer(4, "School")
+Traceback (most recent call last):
+    ...
+TypeError: b must be an integer
+>>> add_integer(None)
+Traceback (most recent call last):
+    ...
+TypeError: a must be an integer
+"""
 
 def add_integer(a, b=98):
     """
@@ -14,13 +31,10 @@ def add_integer(a, b=98):
 
     Raises:
         TypeError: If a or b is not an integer or float.
-        ValueError: If a or b is NaN.
-        OverflowError: If a or b is infinity.
 
     Returns:
         int: Sum of a and b as integers.
     """
-
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
@@ -31,9 +45,17 @@ def add_integer(a, b=98):
             raise ValueError("a cannot be NaN")
         if a > 1e308 or a < -1e308:  # Infinity approx check
             raise OverflowError("cannot convert float infinity to integer")
+        a = int(a)
     if isinstance(b, float):
         if b != b:  # NaN check
             raise ValueError("b cannot be NaN")
         if b > 1e308 or b < -1e308:  # Infinity approx check
             raise OverflowError("cannot convert float infinity to integer")
+        b = int(b)
+
     return a + b
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
