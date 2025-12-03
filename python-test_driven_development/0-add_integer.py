e712aabf0ba838d883ede75a2ec71986658619e2
noneals <124405266+noneals@users.noreply.github.com>
2025-12-03 17:43:49 +0300

    Update 0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import math
 
 def add_integer(a, b=98):
@@ -5,17 +6,14 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-
     if isinstance(a, float):
         if math.isnan(a):
             raise ValueError("a cannot be NaN")
         if math.isinf(a):
             raise OverflowError("cannot convert float infinity to integer")
-
     if isinstance(b, float):
         if math.isnan(b):
             raise ValueError("b cannot be NaN")
         if math.isinf(b):
             raise OverflowError("cannot convert float infinity to integer")
-
     return int(a) + int(b)
