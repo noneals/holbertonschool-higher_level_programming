3786510b4bb4fe9a31226c2e9dbde79c0f2c3f17
noneals <124405266+noneals@users.noreply.github.com>
2025-12-16 12:37:02 +0300

    Update 1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
+""" class MyList that inherits from list """
 
 
 class MyList(list):
-    """extended version of list
-    """
+    """ MyList inherits from list """
+
     def print_sorted(self):
-        """prints the list in ascending order
-        """
-        copy = self[:]
-        copy.sort()
-        print(copy)
+        """ print list in ascending order """
+        print(sorted(self))
